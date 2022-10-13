0fee498e0f45341bce8b143fb5212ed92b2dc5b7
kanteer <59344076+Kanteer@users.noreply.github.com>
2022-10-13 09:26:07 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
+let lastRenderTime = 0
+const SNAKE_SPEED = 4
 
+function main(currentTime) {
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondsSincelastRender < 1 / SNAKE_SPEED) return
+  
+  
+  window.requestAnimationFrame(main)
+  lastRenderTime = currentTime
+  
+  update()
+  draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+}
+
+function draw() {
+}
