13f22008294ff22cb8964b099d72547daeb203e2
kanteer <59344076+Kanteer@users.noreply.github.com>
2022-10-13 09:34:54 -0400

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,7 +1,15 @@
 export const SNAKE_SPEED = 4
+const snakeBody = [{ x:11, y: 11 }]
 
 export function update() {
 }
 
-export function draw() {
+export function draw(gameBoard) {
+  snakeBody.forEach(segment => {
+    const snakeElement = document.createElement('div')
+    snakeElement.syle.gridRowStart = segment.x
+    SnakeElement.style.gridColumnStart = segment.y
+    snakeElement.classList.add('snake')
+    gameBoard.appendChild(snakeElement)
+  })
 }
