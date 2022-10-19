4d6c3c9276a4984e67c447d1c31b474fee9cf5a1
kanteer <59344076+Kanteer@users.noreply.github.com>
2022-10-19 10:03:14 -0400

    Update snake.js

--- snake.js
+++ snake.js
@@ -2,13 +2,18 @@ export const SNAKE_SPEED = 4
 const snakeBody = [{ x:11, y: 11 }]
 
 export function update() {
+  for (let i = snakeBody.length - 2; i => 0; i --) {
+    snakeBody[i + 1] = { ...snakeBody[i] }
+  }
+  snakeBody[0].x += 1
+  snakeBody[0].y += 0
 }
 
 export function draw(gameBoard) {
   snakeBody.forEach(segment => {
     const snakeElement = document.createElement('div')
-    snakeElement.syle.gridRowStart = segment.x
-    SnakeElement.style.gridColumnStart = segment.y
+    snakeElement.syle.gridRowStart = segment.y
+    SnakeElement.style.gridColumnStart = segment.x
     snakeElement.classList.add('snake')
     gameBoard.appendChild(snakeElement)
   })
